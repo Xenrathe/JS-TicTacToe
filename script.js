0441a90d9a631c94221b4ce305e6b6bf217ab035
Xenrathe <126534032+Xenrathe@users.noreply.github.com>
2024-08-07 21:08:11 -0400

    Add HTML and CSS to bring game onto actual page

--- script.js
+++ script.js
@@ -5,31 +5,20 @@ function createPlayer (name, icon) {
 
 //Initialize the game object, of which there will only ever be one
 const game = (function () {
-  let players = [];
+  //Setup 'private' variables
   let board = [[null, null, null],[null, null, null], [null, null, null]];
   let firstPlayerTurn = true;
+  let gameWinner = null;
+  let players = [];
+  players.push(createPlayer('Player One', 'X'));
+  players.push(createPlayer('Player Two', 'O'));
 
-  const addPlayer = (name, icon) => {
-    players.push( createPlayer(name, icon) );
-  }
+  //Connect to DOM
+  const playerDivs = document.querySelectorAll('.player');
+  const announcements = document.querySelector('#announcements');
+  const squares = document.querySelectorAll('.square');
 
-  const makePlay = (col, row) => {
-    if ( board[col][row] != null ) {
-      console.log('Error: Space already taken');
-      return false;
-    }
-    else if ( col >= board.length || row >= board[0].length ) {
-      console.log('Error: Row or col outside of board');
-      return false;
-    }
-    else {
-      let playerTurnNum = firstPlayerTurn ? 0 : 1;
-      board[col][row] = players[playerTurnNum].icon;
-      firstPlayerTurn = !firstPlayerTurn;
-      return true;
-    }
-  }
-
+  //'Private' function returns 'X', 'O', 'Tie', or null, depending on win status
   const checkWinner = () => {
     // Check rows
     for (let i = 0; i < 3; i++) {
@@ -63,12 +52,88 @@ const game = (function () {
     return null;
   }
 
+  //'private' function updates the #announcements div
+  const updateAnnouncements = () => {
+    if (gameWinner == 'X'){
+      announcements.innerHTML = `${players[0].name} has won!`
+    }
+    else if (gameWinner == 'O') {
+      announcements.innerHTML = `${players[1].name} has won!`
+    }
+    else if (gameWinner == 'Tie') {
+      announcements.innerHTML = "Game is a tie!"
+    }
+    else {
+      let playerTurnNum = firstPlayerTurn ? 0 : 1; 
+      announcements.innerHTML = `${players[playerTurnNum].name}'s turn`
+    }
+  }
+
+  const changePlayerName = (num) => {
+    let name = '';
+    while (name == '' || name.length > 10 ) {
+      name = prompt("Enter your name (1-10 characters):");
+    }
+
+    // This error should never get triggered but hey, let's check anyway!
+    if (num < 0 || num > 1){
+      console.log("Error: Player does not exist");
+      return false;
+    }
+    else {
+      playerDivs[num].querySelector('.name').innerHTML = name;
+      players[num].name = name;
+      updateAnnouncements();
+
+      return true;
+    }
+  }
+
+  const makePlay = (col, row) => {
+
+    //Prevent makePlay if the game is currently 'over'
+    if (gameWinner != null){
+      console.log('Error: Game is already over');
+      return false;
+    }
+
+    if ( board[col][row] != null ) {
+      console.log('Error: Space already taken');
+      return false;
+    }
+    else if ( col >= board.length || row >= board[0].length ) {
+      console.log('Error: Row or col outside of board');
+      return false;
+    }
+    else {
+      let playerTurnNum = firstPlayerTurn ? 0 : 1;
+      board[col][row] = players[playerTurnNum].icon;
+      
+      firstPlayerTurn = !firstPlayerTurn;
+
+      //Adjust display / DOM
+      squares[col*3 + row].innerHTML = players[playerTurnNum].icon;
+      squares[col*3 + row].classList.add(players[playerTurnNum].icon)
+
+      //Check for win
+      gameWinner = checkWinner();
+      updateAnnouncements();
+      return true;
+    }
+  }
+
   const resetGame = (includingPlayers) => {
+    gameWinner = null;
     firstPlayerTurn = true;
     board = [[null, null, null],[null, null, null], [null, null, null]];
     if (includingPlayers) {
       players = [];
     }
+    squares.forEach((square) => {
+      square.classList.remove('X');
+      square.classList.remove('O');
+      square.innerHTML = '';
+    });
   }
 
   const updateDisplay = (consoleOnly) => {
@@ -79,8 +144,11 @@ const game = (function () {
         if (i < 2) console.log('---------');
       }
     }
+    else {
+
+    }
   }
 
-  return { players, board, addPlayer, makePlay, checkWinner, resetGame, updateDisplay }
+  return { changePlayerName, makePlay, resetGame, updateDisplay }
 })();
 
